16ce49bd71e581b22f7ea40afb01c9950175148d
Bug Blitz <84833115+mannaweb@users.noreply.github.com>
2023-12-27 12:05:17 +0530

    up

--- next.config.js
+++ next.config.js
@@ -2,11 +2,33 @@
 const path = require("path");
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: false,
+ // swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+  },images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sangita.tech',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
+
 };
 
 module.exports = nextConfig;
