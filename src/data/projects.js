1c0da8a1c53358cee046396504afde09f9d3862f
Sangita272 <100749464+Sangita272@users.noreply.github.com>
2024-01-09 13:04:35 +0530

    up

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ const projects = [
     sceenshots: [],
     githubUrl: "#",
     playstoreUrl: "#",
-    url: "https://dabbawala.iosx.in/",
+    url: "https://res.cloudinary.com/dop8490w6/image/upload/v1704784895/16735977892017d8a2580d7b53b8f86fd5f79_telwwb.png",
     repoType: "public",
     tags: ["Node.js", "Express.js", "MongoDB", "WebSocket", "React Js"],
   },
@@ -15,7 +15,7 @@ const projects = [
     title: "Ajiji",
     description:
       "Ajiji serves as a comprehensive guide on a journey of self-discovery, personal growth, and unparalleled success. Through a carefully crafted program, Ajiji provides users with a powerful toolkit, comprising resources, strategies, and personalized insights tailored to their unique goals and aspirations. This project is designed to empower individuals across various aspects of their lives, be it career advancement, relationship improvement, mindfulness cultivation, or overall well-being enhancement.",
-    icon: "/logo/ajiji.webp",
+    icon: "https://res.cloudinary.com/dop8490w6/image/upload/v1704692357/ajiji_iminnt.png",
     sceenshots: [],
     githubUrl: "#",
     playstoreUrl: "#",
@@ -27,7 +27,7 @@ const projects = [
     title: "Faithly",
     description:
       "The project is a Bible study platform featuring a detailed exploration of books, chapters, verses, and topics. Users can earn coins by completing quizzes at various levels tied to Bible content. With support for multiple user accounts, including child-friendly profiles, it encourages collaborative learning through groups and chats. The platform also integrates Stripe for subscription-based access, enhancing the overall Bible study experience.",
-    icon: "/logo/flutter.webp",
+    icon: "https://res.cloudinary.com/dop8490w6/image/upload/v1704784166/logo.4b514c48_aror0j.png",
     sceenshots: [],
     githubUrl: "#",
     playstoreUrl: "https://play.google.com/store/apps/details?id=com.FaithyApp&hl=en&gl=US&pli=1",
@@ -39,10 +39,10 @@ const projects = [
     title: "CEO",
     description:
       "The CEO Project is a visionary initiative that brings together a select group of elite users, specifically CEOs of companies, fostering a unique space for them to exchange invaluable insights. This exclusive platform serves as a confidential and collaborative environment where CEOs can share their wealth of knowledge, experiences, and strategic perspectives. By facilitating meaningful interactions among top executives, the CEO Project aims to cultivate a community that transcends traditional boundaries, enabling its members to stay ahead of industry trends, tackle challenges collectively, and contribute to each other's professional growth. ",
-    icon: "/logo/reactjs.webp",
+    icon: "https://res.cloudinary.com/dop8490w6/image/upload/v1704785311/z7qe7fdlolkfygg0fdnlona2vu0q_glrosn.png",
     sceenshots: [],
     githubUrl: "#",
-    url: "https://ceofront.iosx.in/",
+    url: "https://www.ceo.com/",
     repoType: "public",
     tags: ["React.js", "Redux", "Material UI"],
   },
@@ -50,7 +50,7 @@ const projects = [
     title: "Inloop",
     description:
       "The InLoop Project is a comprehensive solution designed to streamline issue resolution and task management within an organization. Catering to users, technicians, and administrators, the platform offers a user-friendly login system that provides tailored access based on predefined permissions. Users can effortlessly log technical issues and assign them to designated technicians, creating a seamless workflow for problem resolution. The dynamic form generation feature allows users to articulate issues effectively, ensuring clarity for technicians in their work. Crucially, the project incorporates a robust permission system, delineating access levels for different roles—users, technicians, and administrators—ensuring the right individuals have access to pertinent modules.",
-    icon: "/logo/flutter.webp",
+    icon: "https://res.cloudinary.com/dop8490w6/image/upload/v1704784955/logo_2_iuiid3.png",
     sceenshots: [],
     githubUrl: "#",
     url: "https://inloopapp.iosx.in/auth/login",
@@ -61,7 +61,7 @@ const projects = [
     title: "Invenstory",
     description:
       "It help businesses of every size save money, time and stress with intuitive inventory management software. We help businesses of every size save money, time and stress with intuitive inventory management software.",
-    icon: "/logo/nodejs.webp",
+    icon: "https://res.cloudinary.com/dop8490w6/image/upload/v1704784443/folder_eiwbk4.png",
     sceenshots: [],
     githubUrl: "#",
     url: "https://invenstory.com/",
@@ -72,7 +72,7 @@ const projects = [
     title: "LearningGt",
     description:
       "LearninGT, a project by Grant Thornton in India, is a premier upskilling service offering a diverse range of training programs. Tailored to the dynamic demands of the professional landscape, the academy is dedicated to empowering both seasoned professionals and students. With a commitment to excellence, LearninGT provides comprehensive education to enhance skills, foster growth, and unlock the full potential of individuals in their respective fields.",
-    icon: "/logo/flutter.webp",
+    icon: "https://res.cloudinary.com/dop8490w6/image/upload/v1704785084/main-logo-alt_lyaxph.png",
     sceenshots: [],
     githubUrl: "#",
     url: "https://www.learningt.com/",
@@ -83,7 +83,7 @@ const projects = [
     title: "BusinessIntelligence",
     description:
       "The Business Intelligence project is a comprehensive platform designed to deliver timely news updates, an extensive glossary of industry terms, and interactive forums for insightful discussions. This multifaceted platform caters to users seeking a centralized hub for staying informed about business-related developments, understanding complex terminologies, and engaging in meaningful conversations within a community of professionals. By seamlessly integrating these components, the Business Intelligence project provides a valuable resource for individuals and businesses aiming to enhance their knowledge, stay current with industry trends, and participate in collaborative learning and discussions.",
-    icon: "/logo/flutter.webp",
+    icon: "https://res.cloudinary.com/dop8490w6/image/upload/v1704785296/bih_crqy5r.png",
     sceenshots: [],
     githubUrl: "#",
     url: "https://businessintelligencetools.com/",
